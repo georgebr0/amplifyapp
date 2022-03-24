c5fce53dc2b03444f95e6f3ab852ef0d3a2ba6ca
georgebr0 <53833312+georgebr0@users.noreply.github.com>
2022-03-24 15:50:02 -0300

    Mensagem editada pro monstro

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Salve Monstro, react rodando online, chamaaaa!.
         </p>
         <a
           className="App-link"
